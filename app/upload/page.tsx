040d38a567df09bc1147d75429b4a67869dbd3f0
Shanmukh459 <saikrrish459@gmail.com>
2024-09-16 16:17:39 -0700

    customizing upload widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,39 @@
 "use client"
-import React from "react"
-import { CldUploadWidget } from "next-cloudinary"
+import React, { useState } from "react"
+import { CldImage, CldUploadWidget } from "next-cloudinary"
+
+interface CloudinaryResult {
+  public_id: string
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("")
   return (
-    <CldUploadWidget uploadPreset="pil07nrx">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="Charminar" />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        uploadPreset="pil07nrx"
+        onUpload={(result, widget) => {
+          console.log(result)
+          if (result.event !== "success") return
+          const info = result.info as CloudinaryResult
+          setPublicId(info.public_id)
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   )
 }
 
