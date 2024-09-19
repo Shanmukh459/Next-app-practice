2e3ccdb22b36283f504e80bd57beb9497dc00968
Shanmukh459 <saikrrish459@gmail.com>
2024-09-19 16:47:38 -0700

    configuring google provider for authentication

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,11 +3,12 @@ import React from "react"
 
 const NavBar = () => {
   return (
-    <div className="flex bg-sky-200 p-4">
+    <div className="flex bg-sky-200 p-4 space-x-3">
       <Link href="/" className="mr-4">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      <Link href="api/auth/signin">Login</Link>
     </div>
   )
 }
