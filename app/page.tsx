89d6e65555f4b8324c0ed353153f68a359a86ed3
Shanmukh459 <saikrrish459@gmail.com>
2024-10-02 12:46:38 -0700

    understading image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,29 @@ import Link from "next/link"
 import ProductCard from "./components/ProductCard"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./api/auth/[...nextauth]/route"
+import Image from "next/image"
+import icecream from "@/public/images/icecream.jpg"
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  // const session = await getServerSession(authOptions)
+  // return (
+  //   <main>
+  //     <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+  //     <Link href="/users">Users</Link>
+  //     <ProductCard />
+  //   </main>
+  // )
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="Ice cream"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   )
 }
