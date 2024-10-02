a7439f41e9ae6590d39625187d964384a88ed67a
Shanmukh459 <saikrrish459@gmail.com>
2024-10-01 17:21:34 -0700

    styling emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { CSSProperties } from "react"
 import {
   Body,
   Container,
@@ -6,20 +6,31 @@ import {
   Link,
   Preview,
   Text,
+  Tailwind,
 } from "@react-email/components"
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}!</Text>
-          <Link href="www.google.com">www.google.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-blue-300">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}!</Text>
+            <Link href="www.google.com">www.google.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   )
+}
+
+const body: CSSProperties = {
+  background: "lightblue",
+}
+
+const heading: CSSProperties = {
+  fontSize: "32px",
 }
 
 export default WelcomeTemplate
