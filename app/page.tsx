74c031b6cbdb03715c7e12498c97e78517e11610
Shanmukh459 <saikrrish459@gmail.com>
2024-10-02 17:36:15 -0700

    understanding lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,40 @@
+"use client"
+
 import Link from "next/link"
 import ProductCard from "./components/ProductCard"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./api/auth/[...nextauth]/route"
 import Image from "next/image"
 import icecream from "@/public/images/icecream.jpg"
+import { useState } from "react"
 
-export default async function Home() {
-  const session = await getServerSession(authOptions)
+export default function Home() {
+  const [isVisible, setVisible] = useState(false)
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <h1>Hello</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }]
+          const sortedUsers = _.orderBy(users, ["name"])
+          console.log(sortedUsers)
+        }}
+      >
+        Show
+      </button>
     </main>
   )
+
+  // const session = await getServerSession(authOptions)
+  // return (
+  //   <main>
+  //     <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+  //     <Link href="/users">Users</Link>
+  //     <ProductCard />
+  //   </main>
+  // )
+
   // return (
   //   <main className="relative h-screen">
   //     <Image
